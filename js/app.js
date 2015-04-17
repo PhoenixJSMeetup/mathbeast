8ae3cd28fd8697b55d66cdc3e00eee66ed27a39b
Maximo Mena <maximo.mena@gmail.com>
2015-04-16 23:30:09 -0500

    Refactoring javascript to use MathBeast object.

--- js/app.js
+++ js/app.js
@@ -3,19 +3,67 @@
    * Settings
    */
   // Show and hide settings
-  $('.settings-button').click(function() {
-      $('.settings').fadeToggle();
-    }
-  );
+  //$('.settings-button').click(function() {
+  //    $('.settings').fadeToggle();
+  //  }
+  //);
+
+  // Use JQuery UI to replace operator dropdown with selectmenu widget
+  $('#operation').buttonset();
 
   // Start game
   $('.settings-save').click(function() {
-    var Level1 = new Level(3, 4);
-    Level1.init();
+    var upperLimit = $('#upper-limit').val();
+    var operation = $('#operation .ui-state-active span').text();
+    var totalQuestions = 4;
+
+    // Initialize game
+    MathBeast.start(parseInt(upperLimit), operation, parseInt(totalQuestions));
+
+    // Get first question
+    var question = MathBeast.getCurrentQuestion();
+
+    // Fill in question and answer values on markup
+    $('#question--operand-1').text(question.operand1);
+    $('#question--operation').text(question.operation);
+    $('#question--operand-2').text(question.operand2);
+    $('#answers--option-1').text(question.answerOptions[0]);
+    $('#answers--option-2').text(question.answerOptions[1]);
+    $('#answers--option-3').text(question.answerOptions[2]);
+
+    $('main .instructions').fadeOut();
+    $('main .question').fadeIn();
+    $('main .answers').fadeIn();
   });
 
-  // Use JQuery UI to replace operator dropdown with selectmenu widget
-  $('#operatorInput').selectmenu();
+  // Next Question
+  $('.answers .answer-option').click(function() {
+    // Get current question
+    var result = MathBeast.attempt(parseInt($(this).text()));
+    if (result.rightAnswer) {
+      console.log("Right Answer");
+      if (result.nextQuestion) {
+        var question = result.nextQuestion;
+        // Fill in question and answer values on markup
+        $('#question--operand-1').text(question.operand1);
+        $('#question--operation').text(question.operation);
+        $('#question--operand-2').text(question.operand2);
+        $('#answers--option-1').text(question.answerOptions[0]);
+        $('#answers--option-2').text(question.answerOptions[1]);
+        $('#answers--option-3').text(question.answerOptions[2]);
+      }
+      else {
+        var attempts = MathBeast.attempts;
+        var totalQuestions = MathBeast.settings.totalQuestions;
+        console.log("Game Over!");
+        console.log("Questions: " + totalQuestions);
+        console.log("Attempts: " + attempts);
+      }
+    }
+    else {
+      console.log("Wrong Answer. Try Again");
+    }
+  });
 
 
 }(jQuery));
--- js/mathbeast.js
+++ js/mathbeast.js
@@ -1,76 +1,200 @@
-//consider levels as in platform games like Super Mario. 
-//A constructor here takes two arguments: 
-//arrNumber:how many numbers are displayed 
-//elNumber:how many questions for each level
+/**
+ * Consider levels as in platform games like Super Mario.
+ *
+ * A constructor here takes two arguments:
+ * arrNumber:how many numbers are displayed
+ * elNumber:how many questions for each level
+ */
 
-function Level(arrNumber,elNumber){
-	var upperLimit,lowerLimit,operator,questionArrs,answerArr,userAnswer,
-	    doMath = {
-	    	"+":function(x,y){return x+y},
-	    	"-":function(x,y){return x-y},
-	    	"*":function(x,y){return x*y},
-	    	"/":function(x,y){return x/y}
-	    };
+/**
+ * @namespace Main application object
+ */
+var MathBeast = {
+  // User input
+  settings: {
+    upperLimit: 0,
+    operation: '',
+    totalQuestions: 0
+  },
 
-    //initializing the level object by assigning values to the key variables
-     this.init = function(){
-    	upperLimit = document.getElementById("upperLimitInput").value;
-        lowerLimit = document.getElementById("lowerLimitInput").value;
+  // State variables
+  questions: [],
+  currentQuestion: 0,
+  attempts: 0,
 
-        operator = document.getElementById("operatorInput").value,
+  /**
+   * Modify default settings to start a new game.
+   *
+   * @param {number} Upper limit for operands
+   * @param {string} The operation
+   * @param {number} Number of questions
+   */
+  start: function(upperLimit, operation, totalQuestions) {
+    var validOptions = true;
 
-    //returns an array of arrays containing random numbers
-        questionArrs = (function(arr, el) {
-	        var i = 0,
-	            result = [];
-	        for (i; i < arrNumber; i++) {
-	            var j=0,
-	                arr =[];
-	            for (j; j < elNumber; j++) {
-	                var el,
-	                    upper = (upperLimit? upperLimit:10),
-	                    lower = (lowerLimit? lowerLimit:1);
-	                el = Math.round(Math.random() * (upper - lower) + lower);
-	                arr.push(el);
-	            }
-	            result.push(arr);
-	        }
-	        return result;
-	    })(arrNumber,elNumber);
-
-     //returns an array containing the answer numbers
-	    answerArr = (function (quesArrs) {
-		    var i = 0,
-		        reArrange=[],
-		        result = [],
-		        arrLength = quesArrs.length,
-		        elLength = quesArrs[0].length;
-
-		    for (i; i < arrLength; i++) {
-		        var j = 0,
-		            arr = [];
-		        for (j; j < elLength; j++) {
-		            arr.push(quesArrs[j][i])
-		        }
-		        reArrange.push(arr)
-		    };
-		    
-		    reArrange.map(function (elementArr) {
-		        var answer = elementArr.reduce(function (prev, current) {
-		            return doMath[operator](prev, current)
-		        })
-		        result.push(answer)
-		    });
-
-		    return result;
-
-		})(questionArrs);
-
-	this.getUserAnswer = function(){
-		userAnswer = document.getElementById("userAnswerInput").value;
-	};
-
+    // Validate arguments
+    if (!(typeof upperLimit === 'number' && (upperLimit % 1) === 0 && upperLimit > 0) ||
+      !(typeof totalQuestions === 'number' && (totalQuestions % 1) === 0) && totalQuestions > 0) {
+      validOptions = false;
+      throw {
+        name: "invalidInteger",
+        message: "Both Upper Limit and Total Questions need to be integers greater than 0."
+      };
     }
 
+    if ((operation !== '+') && (operation !== '-') && (operation !== '*') &&
+      (operation !== '/')) {
+      validOptions = false;
+      throw {
+        name: "invalidOperation",
+        message: "The operation is invalid. It needs to be one of the following:" +
+        "+, -, * or /"
+      };
+    }
 
-}
+    if (validOptions) {
+      this.settings.upperLimit = upperLimit;
+      this.settings.operation = operation;
+      this.settings.totalQuestions = totalQuestions;
+      this.currentQuestion = 0;
+      this.attempts = 0;
+      this.questions = this.getQuestions();
+    }
+
+  },
+
+  /**
+   * Collect all questions
+   *
+   * @return {array} All questions and answers in an array of objects
+   */
+  getQuestions: function() {
+    var questions = [];
+
+    for (var i = 0; i < this.settings.totalQuestions; i++) {
+      questions[i] = this.getQuestion();
+    }
+
+    return questions;
+  },
+
+  /**
+   * Create a question and answer options
+   *
+   * @return {object} Question and answer options
+   */
+  getQuestion: function() {
+    var operand1 = this.getOperand();
+    var operand2 = this.getOperand();
+    var operation = this.settings.operation;
+    var answer = this.getAnswer(operand1, operand2);
+    var answerOptions = this.getAnswerOptions(answer);
+
+    return {
+      operand1: operand1,
+      operand2: operand2,
+      operation: operation,
+      answer: answer,
+      answerOptions: answerOptions
+    };
+
+  },
+
+  /**
+   * Get current question
+   *
+   * @return {object} Question and answer options
+   */
+  getCurrentQuestion: function() {
+    return this.questions[this.currentQuestion];
+  },
+
+  /**
+   * Get current question
+   *
+   * @return {object} Question and answer options
+   */
+  getNextQuestion: function() {
+    var next = false;
+
+    // Check if there is a next question
+    if (this.currentQuestion < (this.questions.length - 1)) {
+      // Move iterator by one
+      this.currentQuestion = this.currentQuestion + 1;
+      next = this.questions[this.currentQuestion];
+    }
+
+    return next;
+  },
+
+  /**
+   * Get current question
+   *
+   * @param {number}
+   * @return {object} Question and answer options
+   */
+  attempt: function(attemptedAnswer) {
+    // Increment number of attempts
+    this.attempts = this.attempts + 1;
+
+    // Assume wrong answer
+    var result = {
+      rightAnswer: false,
+      nextQuestion: false
+    };
+
+    // Get current question
+    currentQuestion = this.getCurrentQuestion();
+
+    // Check if attempted answer matches correct answer
+    if (attemptedAnswer === currentQuestion.answer) {
+      // Update result type
+      result.rightAnswer = true;
+
+      // Get next question, if any
+      var nextQuestion = this.getNextQuestion();
+
+      // If game is not over, update nextQuestion
+      if (nextQuestion !== false) {
+        result.nextQuestion = nextQuestion;
+      }
+    }
+
+    return result;
+  },
+
+  /**
+   * Random number greater than or equal to 0 and less than or equal to upper limit
+   *
+   * @return {number} Random number
+   */
+  getOperand: function() {
+    // TODO
+    return 5;
+  },
+
+  /**
+   * Perform math operation and return the result
+   *
+   * @param {number} First operand
+   * @param {number} Second operand
+   * @return {number} Result of math calculation
+   */
+  getAnswer: function(operand1, operand2) {
+    // TODO
+    return 10;
+  },
+
+  /**
+   * Return numbers that are close to correct result
+   *
+   * @param {number} Upper limit for operands
+   * @return {array} List of one right answer and two wrong answers
+   */
+  getAnswerOptions: function() {
+    // TODO
+    return [7, 10, 11];
+  }
+
+};
+
