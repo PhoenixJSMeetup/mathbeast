ba4701274484bfd581b277e3d20d646eae0341ef
Maximo Mena <maximo.mena@gmail.com>
2015-07-02 07:28:35 -0700

    Fix indentation on appView.js. Setting upperLimit to 12 and totalQuestions to 4.

--- js/views/appView.js
+++ js/views/appView.js
@@ -1,106 +1,109 @@
 define([
-    'jquery',
-    'underscore',
-    'backbone',
-    'MathBeast',
-    'views/appView',
-    'views/settingView',
-    'views/mainView',
-    'text!templates/AppView.html'
+  'jquery',
+  'underscore',
+  'backbone',
+  'MathBeast',
+  'views/appView',
+  'views/settingView',
+  'views/mainView',
+  'text!templates/AppView.html'
 ], function($, _, Backbone,MathBeast,AppView,SettingView,MainView,template) {
 
-	var AppView = Backbone.View.extend({
+  var AppView = Backbone.View.extend({
 
-		el:$("#mathBeast-container"),
+    el:$("#mathBeast-container"),
 
-		template:template,
+    template:template,
 
-		initialize:function(operation,RegionView){
-			var _this = this;
-			this.$el.html(_this.template);
-            this.operation = operation;
-		    this.renderMainView();
-			$("body").on("rightAnswer",function(event,data){
-				_this.countDown.clear().run()
-			})
-			$("body").on("end",function(){
-				_this.countDown.clear();
-                //logic for determining passing
-                switch(_this.operation){
-                    case '+':
-                        new RegionView('one');
-                        break;
-                    case '-':
-                        new RegionView('two');
-                        break;
-                    case '*':
-                        new RegionView('three');
-                        break;
-                    case '/':
-                        new RegionView('four');
-                        break;
-                }
-			})
-		},
-		renderMainView:function(){
-			var myMainView = new MainView({
-				el:$("#mainContainer")
-			})
-			this.startGame();
-		},
-		startGame:function(){
-			var upperLimit = 1;
-		    var operation = this.operation;
-            var totalQuestions = 1;
+    initialize:function(operation,RegionView){
+      var _this = this;
+      this.$el.html(_this.template);
+      this.operation = operation;
+      this.renderMainView();
+      $("body").on("rightAnswer",function(event,data){
+        _this.countDown.clear().run()
+      })
+      $("body").on("end",function(){
+        _this.countDown.clear();
+        //logic for determining passing
+        switch(_this.operation){
+          case '+':
+              new RegionView('one');
+              break;
+          case '-':
+              new RegionView('two');
+              break;
+          case '*':
+              new RegionView('three');
+              break;
+          case '/':
+              new RegionView('four');
+              break;
+        }
+      })
+    },
 
-		    // Initialize game
-		    MathBeast.start(parseInt(upperLimit), operation, parseInt(totalQuestions));
+    renderMainView:function(){
+      var myMainView = new MainView({
+        el:$("#mainContainer")
+      })
+      this.startGame();
+    },
 
-		    // Get first question
-		    var question = MathBeast.getCurrentQuestion();
+    startGame:function(){
+      var upperLimit = 12;
+      var operation = this.operation;
+      var totalQuestions = 4;
 
-		    // Question Number
-		    $('#question--number').text(1);
+      // Initialize game
+      MathBeast.start(parseInt(upperLimit), operation, parseInt(totalQuestions));
 
-		    // Fill in question and answer values on markup
-		    $('#question--operand-1').text(question.operand1);
-		    $('#question--operation').text(question.operation);
-		    $('#question--operand-2').text(question.operand2);
-		    $('#answers--option-1').text(question.answerOptions[0]);
-		    $('#answers--option-2').text(question.answerOptions[1]);
-		    $('#answers--option-3').text(question.answerOptions[2]);
+      // Get first question
+      var question = MathBeast.getCurrentQuestion();
 
-		    // Enable multiple choice buttons
-		    $('#answers--option-1').removeAttr('disabled');
-		    $('#answers--option-2').removeAttr('disabled');
-		    $('#answers--option-3').removeAttr('disabled');
+      // Question Number
+      $('#question--number').text(1);
 
-		    $('main .status').fadeOut();
-		    $('main .question').fadeIn();
-		    $('main .answers').fadeIn();
-		    $('main .timer').fadeIn();
+      // Fill in question and answer values on markup
+      $('#question--operand-1').text(question.operand1);
+      $('#question--operation').text(question.operation);
+      $('#question--operand-2').text(question.operand2);
+      $('#answers--option-1').text(question.answerOptions[0]);
+      $('#answers--option-2').text(question.answerOptions[1]);
+      $('#answers--option-3').text(question.answerOptions[2]);
 
-		    this.countDown.run();
-		},
-		countDown:{
-			run: function() {
-	      		var newTimer = $("<div class='bar'></div>");
-	      		$("#timerContainer").append(newTimer);
-	      		var time = MathBeast.settings.timer;
-	      		$(".bar").animate({
-	        		"width": "0",
-	        		"padding": "0"
-	      		}, time, function() {
-	        	this.remove();
-	      		})
-	    	},
+      // Enable multiple choice buttons
+      $('#answers--option-1').removeAttr('disabled');
+      $('#answers--option-2').removeAttr('disabled');
+      $('#answers--option-3').removeAttr('disabled');
 
-	    	clear: function() {
-	      		$(".bar").remove();
-	      		return this;
-    			}
-			}
-		});
+      $('main .status').fadeOut();
+      $('main .question').fadeIn();
+      $('main .answers').fadeIn();
+      $('main .timer').fadeIn();
 
-    return AppView;
+      this.countDown.run();
+    },
+
+    countDown:{
+      run: function() {
+        var newTimer = $("<div class='bar'></div>");
+        $("#timerContainer").append(newTimer);
+        var time = MathBeast.settings.timer;
+        $(".bar").animate({
+          "width": "0",
+          "padding": "0"
+        }, time, function() {
+        this.remove();
+        })
+      },
+
+      clear: function() {
+          $(".bar").remove();
+          return this;
+      }
+    }
+  });
+
+  return AppView;
 });
