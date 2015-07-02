32db2ffeddea72c817d17e6370074dd7997ce2a4
Andrew Fiedler <andrewkfiedler@gmail.com>
2015-07-01 18:18:04 -0700

    MVP 3

--- js/views/introductionView.js
+++ js/views/introductionView.js
@@ -4,8 +4,9 @@ define([
     'backbone',
     'MathBeast',
     'views/introductionView',
-    'text!templates/introductionView.html'
-], function ($, _, Backbone, MathBeast, IntroductionView, template) {
+    'text!templates/introductionView.html',
+    'views/regionView'
+], function ($, _, Backbone, MathBeast, IntroductionView, template, regionView) {
 
     var IntroductionView = Backbone.View.extend({
 
@@ -16,15 +17,25 @@ define([
         initialize: function () {
             var _this = this;
             this.$el.html(_this.template);
+            this.playAnimation();
+            this.$el.find('#introduction > .begin > button').on('click',function(){
+                new regionView('none');
+            });
+        },
+        playAnimation: function(){
+            var _this = this;
             setTimeout(function () {
                 _this.$el.find('#introduction').addClass('play');
                 setTimeout(function () {
                     _this.$el.find('#introduction').addClass('explosion');
                     setTimeout(function () {
                         _this.$el.find('#introduction').addClass('welcome');
+                        setTimeout(function () {
+                            _this.$el.find('#introduction').addClass('begin');
+                        }, 15000);
                     }, 1000);
                 }, 3000);
-            }, 500);
+            }, 500);   
         }
     });
 
